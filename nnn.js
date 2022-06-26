4bdd79c645d527a7403b93c04b0213af32bd1c30
t24whjr <96810044+t24whjr@users.noreply.github.com>
2022-06-26 15:54:46 +0530

    Add files via upload

---
+++ nnn.js
@@ -0,0 +1,15 @@
+var box;
+
+function setup(){
+  createCanvas(600,200);
+  box= createSprite(200,200,30,30);
+
+}
+
+
+function draw(){
+  //set background color 
+  background("white");
+ 
+  drawSprites();
+}
